07d21087bab8b6762ea0d920012c1059c85e3fcf
Lim Hng Yi <limhngyi@Lims-MacBook-Pro.local>
2023-10-07 21:20:50 +0800

    assignment 3

---
+++ assignment_3/target/debug/deps/assignment_3-da57c35e9e35087f.d
@@ -0,0 +1,5 @@
+/Users/limhngyi/Developer/Rust Course Metacamp/assignment_submission/assignment_3/target/debug/deps/assignment_3-da57c35e9e35087f: src/main.rs
+
+/Users/limhngyi/Developer/Rust Course Metacamp/assignment_submission/assignment_3/target/debug/deps/assignment_3-da57c35e9e35087f.d: src/main.rs
+
+src/main.rs:
